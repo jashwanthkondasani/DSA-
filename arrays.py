6b2e0cce2066bda0224e0ad927f3e5803a6cafc6
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2026-01-07 10:28:44 +0530

    new

--- arrays.py
+++ arrays.py
@@ -1,5 +1,65 @@
-print("Hello, World!")
+# arr=[1, 2, 3, 4, 5]
+# for i in arr:
+#   print(i)
+
+# arr=[10,20,30,40,50]
+# print("sum:",sum(arr))
+# print("max:",max(arr))
+# print("min:",min(arr))
+
+# arr=[50,60,70,80,90]
+# arr.reverse()
+# print(arr)
+
+# arr=[4,5,40,50]
+# even=odd=0
+# for i in arr:
+#   if i%2==0:
+#     even +=1
+#   else:
+#     odd +=1
+# print("even :" ,even ,"odd :",odd)
+
+# arr=[1,2,3,4,5,6,7,8,1,9]
+# seen=set()
+# for i in arr:
+#   if i in seen:
+#     print("duplicate:",i)
+#   seen.add(i)
+
+#  largest element in arraay
+# def largest_element(arr):
+#    largest=arr[0]
+#    for i in range(1,len(arr)):
+#       if arr[i]>largest:
+#           largest=arr[i]
+#    return largest
+# arr=[2,5,67,88,77]
+# print("largest element is:",largest_element(arr))
+
+# arr=[33,54,43,65,99]
+# print("second largest element is:",second_largest_element(arr))
 
 
-print("Welcome to the Python programming world.")
-print("This is a simple script to demonstrate printing in Python.")
+# second largest element in array 
+# arr=[12,34,67,97,64]
+# largest=-1
+# second_largest=-1
+# for i in range(len(arr)):
+#     if arr[i]>largest:
+#         second_largest=largest
+#         largest=arr[i]
+#     elif arr[i]<largest and arr[i]>second_largest:
+#         second_largest=arr[i]
+# print("second largest element is:",second_largest)
+
+arr=[12,34,67,97,64]
+largest=-1
+third_largest=-1
+for i in range(len(arr)):
+    if arr[i]>largest:
+        third_largest=largest
+        largest=arr[i]
+    elif arr[i]<largest and arr[i]>third_largest:
+        third_largest=arr[i]
+print("third largest element is:",third_largest)
