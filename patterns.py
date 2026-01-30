68067c533d9f8c10ead8691a695bc3f096a44fb4
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2026-01-30 22:44:24 +0530

    DSA

--- patterns.py
+++ patterns.py
@@ -35,8 +35,14 @@
 #         print(j,end="")
 #     print()
 
-n=4
+# n=4
+# for i in range(1,n + 1):
+#     for j in range(i):
+#         print(chr(65+j),end="")
+#     print()
+n=5
 for i in range(1,n + 1):
-    for j in range(i):
-        print(chr(65+j),end="")
-    print()
+    print("*"*i+" "*(2*(n-i))+"*"*i)
+
+for j in range(n,0,-1):
+    print("*"*i+" "*(2*(n-i))+"*"*i)
